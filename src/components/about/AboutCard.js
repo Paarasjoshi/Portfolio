492ff2f9e0e84aeda816a46e86f600fe076ed55f
Paras Joshi <parasjoshi2006@gmail.com>
2025-01-05 09:46:48 +0530

    Add files via upload

---
+++ src/components/about/AboutCard.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Paras Joshi </span>
+            from <span className="purple"> Uttarakhand, India.</span>
+            <br />
+            I am currently in my final year of my Undergraduation.
+            <br />
+            I have completed my B.Tech in Computer Science and Engineering from Graphic Era Hill University.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Chess
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Reading Tech Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Connecting to new Communities
+            </li>
+          </ul>
+
+          {/* <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Soumyajit</footer> */}
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
